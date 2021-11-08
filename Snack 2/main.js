b70919b4bfd32ea25d8198d83714a7eaa32e7c3b
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2021-11-08 16:35:17 +0100

    Snack2

---
+++ Snack 2/main.js
@@ -0,0 +1,8 @@
+/* Snack 2
+A partire da un array di numeri, genera un secondo array con le potenze al quadrato di ogni numero.
+Es: [1, 2, 3, 4, 5] => [1, 4, 9, 16, 25] */
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const exponentiation = number.map((number)=> number * number);
+console.log(exponentiation);

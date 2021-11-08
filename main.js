71e424a44e1dbe27c2042f2f35281fee521f3d61
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2021-11-08 15:48:30 +0100

    punto 1-2-3

--- main.js
+++ main.js
@@ -13,3 +13,48 @@
 3. Stampare Nome, cognome, età e codice giocatore.
 4. Creare un array di 10 giocatori di basket, con le regole sopra indicate
 5. Creare un nuovo array con i giocatori che hanno una media di punti superiore a 35 e la percentuale di successo per i tiri da 3 punti superiore all’80%. */
+
+
+/* ogetto dei giocatori */
+const player = {
+
+    codePlayer: letterCodePlayer(),
+    firstName: 'LeBron',
+    lastName: 'James',
+    age: 39,
+    averagePoints: randomNumber(0, 50),
+    percentageShots: randomNumber(0, 100),
+}
+
+
+/* Stampare Nome, cognome, età e codice giocatore */
+const {firstName, lastName, age, codePlayer} = player;
+
+console.log(firstName, lastName, age, codePlayer);
+
+
+
+
+/* FUNCTION */
+
+/* numeri random */
+function randomNumber(min, max) {
+    const points = Math.floor(Math.random() * (max - min + 1))+min;
+    return points;
+}
+
+/* generazione codice giocatore */
+function letterCodePlayer() {
+    let alphabet = "abcdefghiklmnopqrstuvwxyz".toUpperCase();
+    let letterCode = '';
+    let numberCode = '';
+
+    for (let i = 0; i < 3; i++) {
+        letterCode += alphabet[randomNumber(0, alphabet.length)];
+        numberCode += randomNumber(0, 9); 
+    }
+
+    const codeRandom = letterCode + numberCode;
+
+    return codeRandom;
+}
